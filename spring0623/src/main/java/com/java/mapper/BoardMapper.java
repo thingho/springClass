4cc33859f9eda181199eec5cc3cc213ac6e08821
KMH <KMH@DESKTOP-MBD3LU5>
2023-08-31 17:18:39 +0900

    2023년 06월 23일

---
+++ spring0623/src/main/java/com/java/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.java.mapper;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.java.dto.MemberDto;
+
+@Mapper
+public interface BoardMapper {
+	//회원 전체 가져오기
+	ArrayList<MemberDto> memberSelectAll();
+
+	//회원 1명 가져오기
+	MemberDto memberSelectOne(String id);
+}
+
